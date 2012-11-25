7ad2c6e554ccdac162833cb6fa5ed567da0eb915
jasdbeat@gmail.com <jasdbeat@gmail.com@032d5935-0313-d6d3-d5b5-051eb68a0b9c>
2012-11-25 08:09:17 +0000

    agregados generación de estados financieros

--- trunk/SCANF/src/java/com/asecon/controller/util/JsfUtil.java
+++ trunk/SCANF/src/java/com/asecon/controller/util/JsfUtil.java
@@ -1,12 +1,21 @@
 package com.asecon.controller.util;
 
 import java.io.IOException;
+import java.sql.Connection;
 import java.util.List;
+import java.util.Map;
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.model.SelectItem;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.export.ooxml.JRDocxExporter;
+import net.sf.jasperreports.engine.export.ooxml.JRDocxExporterParameter;
+import net.sf.jasperreports.engine.export.ooxml.JRXlsxExporter;
 
 public class JsfUtil {
     
@@ -19,6 +28,10 @@ public class JsfUtil {
     public static final double COEFICIENTE_A_GASTOS = 3288.67946428571;
     public static final double COEFICIENTE_B_GASTOS = -13203524.7331785;
     public static final double COEFICIENTE_C_GASTOS = 13252519737.8008;
+    public static final int PDF_REPORT = 1;
+    public static final int DOCX_REPORT = 2;
+    public static final int XLSX_REPORT = 3;
+    public static final int HTML_REPORT = 4;
 
     public static SelectItem[] getSelectItems(List<?> entities, boolean selectOne) {
         int size = selectOne ? entities.size() + 1 : entities.size();
@@ -92,5 +105,63 @@ public class JsfUtil {
     public static void redirect(String url) throws IOException {
         FacesContext context = FacesContext.getCurrentInstance();
         context.getExternalContext().redirect(url);
+    }
+    
+    /**
+     * Método que genera un reporte
+     *
+     * @param path ubicación del archivo *.jasper
+     * @param fileName nombre del reporte a generalr
+     * @param connection conexión a la base de datos
+     * @param parameters parametros del reporte
+     * @param format formato de salida del reporte
+     * @throws JRException
+     * @throws IOException
+     */
+    public static void generateReport(String path, String fileName, Connection connection, Map parameters, int format) throws JRException, IOException {
+        ServletContext servletContext = (ServletContext) FacesContext.getCurrentInstance().getExternalContext().getContext();
+        String realPath = (String) servletContext.getRealPath(path);
+        JasperPrint jasperPrint = JasperFillManager.fillReport(realPath, parameters, connection);
+        HttpServletResponse httpServletResponse = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
+        ServletOutputStream servletOutputStream;
+        switch (format) {
+            case PDF_REPORT:
+                httpServletResponse.setContentType("application/pdf");
+                httpServletResponse.addHeader("Content-disposition", "attachment; filename=" + fileName + ".pdf");
+                servletOutputStream = httpServletResponse.getOutputStream();
+                JasperExportManager.exportReportToPdfStream(jasperPrint, servletOutputStream);
+                servletOutputStream.flush();
+                servletOutputStream.close();
+                break;
+            case XLSX_REPORT:
+                httpServletResponse.addHeader("Content-disposition", "attachment; filename=" + fileName + ".xlsx");
+                servletOutputStream = httpServletResponse.getOutputStream();
+                JRXlsxExporter xlsxExporter = new JRXlsxExporter();
+                xlsxExporter.setParameter(JRExporterParameter.JASPER_PRINT, jasperPrint);
+                xlsxExporter.setParameter(JRExporterParameter.OUTPUT_STREAM, servletOutputStream);
+                xlsxExporter.exportReport();
+                servletOutputStream.flush();
+                servletOutputStream.close();
+                break;
+            case DOCX_REPORT:
+                httpServletResponse.addHeader("Content-disposition", "attachment; filename=" + fileName + ".docx");
+                servletOutputStream = httpServletResponse.getOutputStream();
+                JRDocxExporter docxExporter = new JRDocxExporter();
+                docxExporter.setParameter(JRExporterParameter.JASPER_PRINT, jasperPrint);
+                docxExporter.setParameter(JRExporterParameter.OUTPUT_STREAM, servletOutputStream);
+                docxExporter.setParameter(JRDocxExporterParameter.OUTPUT_STREAM, servletOutputStream);
+                docxExporter.exportReport();
+                servletOutputStream.flush();
+                servletOutputStream.close();
+                break;
+            default:
+                httpServletResponse.setContentType("application/pdf");
+                httpServletResponse.addHeader("Content-disposition", "attachment; filename=" + fileName + ".pdf");
+                servletOutputStream = httpServletResponse.getOutputStream();
+                JasperExportManager.exportReportToPdfStream(jasperPrint, servletOutputStream);
+                servletOutputStream.flush();
+                servletOutputStream.close();
+                break;
+        }
     }
 }
